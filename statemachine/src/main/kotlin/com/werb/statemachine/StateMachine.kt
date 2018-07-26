6d883ea52308f51412cd606f58f0727ff9871a74
wanbo <werbhelius@gmail.com>
2018-07-26 17:44:58 +0800

    Initial commit

---
+++ statemachine/src/main/kotlin/com/werb/statemachine/StateMachine.kt
@@ -0,0 +1,37 @@
+package com.werb.statemachine
+
+/**
+ * Created by wanbo on 2018/6/11.
+ */
+class StateMachine<State, Transition> {
+
+    private val statesMap = mutableMapOf<State, () -> Unit>()
+    private val transitionsMap = mutableMapOf<State, MutableMap<Transition, State>>()
+
+    private var currentState: State? = null
+        set(value) {
+            field = value
+            statesMap[value]?.invoke()
+        }
+
+    fun initialState(state: State) {
+        currentState = state
+    }
+
+    fun initState(state: State, block: () -> Unit) {
+        statesMap[state] = block
+    }
+
+    fun addState(transition: Transition, fromState: State, toState: State) {
+        val bag = transitionsMap[fromState] ?: mutableMapOf()
+        bag[transition] = toState
+        transitionsMap[fromState] = bag
+    }
+
+    fun executeTransition(transition: Transition) {
+        val fromState = currentState ?: return
+        val toState = transitionsMap[fromState]?.get(transition) ?: return
+        currentState = toState
+    }
+
+}
